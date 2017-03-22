a157c0ee58a6824c55f09b21e9d0f429da9c778d
Niclas Hanraths <niclas.hanraths@tu-berlin.de>
2017-03-22 15:11:57 +0100

    Bugfix: Mistakenly swapped molecularWeights() for meanMolecularWeight(). Reverted!

--- reactor_isoV.m
+++ reactor_isoV.m
@@ -63,7 +63,7 @@ y=z(2:end-1); % mass fractions
 
 set(g,'T',T,'Density',rho,'Y',y); %update gas
 
-Ms  = meanMolecularWeight(g);
+Ms  = molecularWeights(g);
 dy = ydot(g); % = netProdRates(g).*Ms/rho; change in mass fraction
 ui  = (enthalpies_RT(g) - 1) *gasconstant * T ./ Ms; % specific internal energies
 cv  = cv_mass(g); % specific heat
